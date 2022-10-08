970b6526123a10907eee9df5168d21214ce833ff
Yash Kumar <43927153+yashkc2025@users.noreply.github.com>
2022-10-08 22:32:47 +0530

    added a card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
-export default function Card(){
-    return(
-        <div className="card">
-            <img src= "https://cdn.mos.cms.futurecdn.net/p5quSf4dZXctG9WFepXFdR.jpg"/>
-            <div>
-                
-            </div>
-        </div>
+export default function Card() {
+    return (
+        <section className="card">
+            <img src="https://cdn.mos.cms.futurecdn.net/p5quSf4dZXctG9WFepXFdR.jpg" />
+
+            <p>⭐ 5.0(6) USA</p>
+            <p>Life lessons with Katie Zaferes</p>
+        </section>
     );
 }
